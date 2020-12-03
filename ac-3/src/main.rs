4ed613459596f449938df2b493968b78f4decdb4
Chris Rose <offline@offby1.net>
2020-12-03 00:34:29 -0800

    AC3 part 2

--- ac-3/src/main.rs
+++ ac-3/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
+use std::slice::Iter;
 
 fn main() {
     let lines = read_lines("ac-3.txt").expect("Cannot read the file");
@@ -8,10 +9,19 @@ fn main() {
         .filter_map(Result::ok)
         .map(|line| line.as_str().chars().collect())
         .collect::<Vec<Vec<char>>>();
+
+    let t1 = trees_hit(matrix.iter(), 1, 1);
+    let t2 = trees_hit(matrix.iter(), 1, 3);
+    let t3 = trees_hit(matrix.iter(), 1, 5);
+    let t4 = trees_hit(matrix.iter(), 1, 7);
+    let t5 = trees_hit(matrix.iter(), 2, 1);
+    println!("Trees hit: {}", t1 * t2 * t3 * t4 * t5);
+}
+
+fn trees_hit(course: Iter<Vec<char>>, row_step: usize, col_step: usize) -> usize {
     let mut col: usize = 0;
     let mut trees: usize = 0;
-
-    for (row_num, row) in matrix.iter().enumerate() {
+    for row in course.step_by(row_step) {
         match row.get(col % row.len()) {
             Some(val) => {
                 if *val == '#' {
@@ -20,9 +30,9 @@ fn main() {
             }
             None => (),
         }
-        col += 3;
+        col += col_step;
     }
-    println!("Hit {} trees", trees);
+    trees
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
