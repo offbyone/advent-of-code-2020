d32834bed66aa99d89a6f12395a89464ea2c32b3
Chris Rose <offline@offby1.net>
2020-12-07 00:49:12 -0800

    7 Part 2

--- ac-7/src/main.rs
+++ ac-7/src/main.rs
@@ -13,7 +13,7 @@ struct RuleError;
 #[derive(Debug)]
 struct Rule {
     colour: String,
-    contain: Vec<String>,
+    contain: HashMap<String, usize>,
 }
 
 impl FromStr for Rule {
@@ -25,7 +25,12 @@ impl FromStr for Rule {
         let tail = caps.get(2).map_or("", |m| m.as_str());
         let bag_cap_colours = bag_count
             .captures_iter(tail)
-            .map(|cap| cap.get(2).unwrap().as_str().to_string())
+            .map(|cap| {
+                (
+                    cap.get(2).unwrap().as_str().to_string(),
+                    cap.get(1).unwrap().as_str().parse().unwrap(),
+                )
+            })
             .collect();
 
         Ok(Rule {
@@ -57,25 +62,28 @@ fn main() -> std::io::Result<()> {
     let file = File::open(args.value_of("INPUT").unwrap()).unwrap();
     let reader = BufReader::new(file);
 
-    let rules: Vec<Rule> = reader
+    let rules: HashMap<String, Rule> = reader
         .lines()
         .filter_map(Result::ok)
         .map(|p| p.parse())
         .filter_map(Result::ok)
+        .map(|r: Rule| (r.colour.clone(), r))
         .collect();
 
     //println!("{:?}", rules);
 
     let mut reverse: HashMap<String, Vec<String>> = HashMap::new();
-    for rule in rules {
-        for allowed in rule.contain {
-            let refs = reverse.entry(allowed).or_insert(vec![]);
-            refs.push(rule.colour.to_owned());
+    for rule in &rules {
+        for allowed in rule.1.contain.keys() {
+            let refs = reverse
+                .entry(allowed.to_owned().to_string())
+                .or_insert(vec![]);
+            refs.push(rule.0.to_owned());
         }
     }
 
     let mut seen: HashSet<String> = HashSet::new();
-    let mut queue: Vec<String> = vec![pack_colour];
+    let mut queue: Vec<String> = vec![pack_colour.to_owned()];
 
     while queue.len() > 0 {
         let check = queue.pop().unwrap();
@@ -88,10 +96,63 @@ fn main() -> std::io::Result<()> {
         }
     }
 
+    let mut size_caches: HashMap<String, usize> = HashMap::new();
+    let number_of_bags = size_of(&pack_colour, &rules, &mut size_caches);
+
     println!("seen: {:?}", seen);
     println!("total: {}", seen.len());
+    println!("Part 2: number of bags: {}", number_of_bags);
 
     Ok(())
+}
+
+fn size_of(
+    colour: &String,
+    rules: &HashMap<String, Rule>,
+    size_cache: &mut HashMap<String, usize>,
+) -> usize {
+    let rule = match rules.get(colour) {
+        None => return 0,
+        Some(r) => r,
+    };
+
+    match rule.contain.len() {
+        0 => {
+            println!("E: c={}, s=1", colour);
+            0
+        }
+        _ => {
+            // println!("C[{}] contents={:?}", colour, rule.contain);
+            let sub_size: usize = rule
+                .contain
+                .iter()
+                .map(|(target, count)| match size_cache.get(target) {
+                    Some(v) => {
+                        let size = count + (v * count);
+                        // println!(
+                        //     "H: target: {} value={}, count={}, size={}",
+                        //     target, v, count, size
+                        // );
+                        size
+                    }
+                    None => {
+                        let t_rule: &Rule = rules.get(target).unwrap();
+                        let new_size = size_of(&t_rule.colour, rules, size_cache);
+                        let size = count + (new_size * count);
+                        // println!(
+                        //     "M: target: {} value={}, count={}, size={}",
+                        //     target, new_size, count, size
+                        // );
+                        size_cache.insert(target.to_string(), new_size);
+                        size
+                    }
+                })
+                .sum();
+
+            // println!("C[{}]: size={}", colour, sub_size);
+            sub_size
+        }
+    }
 }
 
 #[allow(dead_code)]
